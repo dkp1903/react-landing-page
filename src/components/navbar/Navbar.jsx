1097a77cbc8eac302f1550619ac6f661bec8714e
dkp1903 <dushyantpathak6789@gmail.com>
2022-02-10 19:44:04 +0000

    add nav initalize content and classes

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri'
+import './navbar.css'
+
+// CSS naming convention -> BEM
+
+const Navbar = () => {
+    return (
+        <div className='gpt3__navbar'>
+            <div className="gpt__navbar-links"></div>
+        </div>
+    )
+}
+
+export default Navbar
