d352c7b88be0536ec2537331dc647d68f6292894
dkp1903 <dushyantpathak6789@gmail.com>
2022-02-14 20:14:44 +0000

    add logo and start styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { RiMenu3Line, RiCloseLin } from 'react-icons/ri'
 import './navbar.css'
+import logo from '../../assets/logo.svg'
 
 // CSS naming convention -> BEM
 
 const Navbar = () => {
     return (
         <div className='gpt3__navbar'>
-            <div className="gpt__navbar-links"></div>
+            <div className="gpt__navbar-links">
+                <div className="gpt3__navbar-links_logo">
+                    <img src={logo} alt='logo'/>
+                </div>
+            </div>
         </div>
     )
 }
